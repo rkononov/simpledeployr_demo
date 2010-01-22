c22998fde29c6b12f3e763b3930021b0d6859232
unknown <rkononov@.office.ut.kg>
2010-01-22 10:54:24 +0600

    comment changed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,4 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
 
   # Scrub sensitive parameters from your log
-  # filter_parameter_logging :password
 end
